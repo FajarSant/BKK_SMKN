151f90b0db4699cb3f909ce2c2c5c36dde028f83
FajarSant <4jar100so@gmail.com>
2024-04-01 19:00:18 +0700

    User & Post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const prisma = require("./db")
+const cors = require ("cors")
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 dotenv.config();
 
